14558beb902189cdcbd14370b429dde0fc2785ac
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-12 11:06:58 -0800

    try to fix osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,11 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 
+if [[ ${target_platform} == "osx-arm64" ]]; then
+    export CC_aarch64-apple-darwin = ${CC}
+    export CC=${CC_FOR_BUILD}
+fi
+
 # check licenses
 cargo-bundle-licenses \
     --format yaml \
